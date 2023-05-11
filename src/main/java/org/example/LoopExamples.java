a2ec8efa4d2f2bf85ac53ad2c1e2c57f7bd900c9
Opie Taylor <taborn.nequil@gmail.com>
2023-05-11 09:40:22 -0500

    first

---
+++ src/main/java/org/example/LoopExamples.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class LoopExamples {
+
+    public static void main(String[] args) {
+
+        // print the numbers from 1 to 100
+
+//        for(int num=1; num<=100; num++){
+//            System.out.println(num);
+//        }
+
+        
+
+    }
+}
