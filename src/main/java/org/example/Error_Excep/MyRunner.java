41ba4db67f359580ef9c48b79fa39609a6782918
Opie Taylor <taborn.nequil@gmail.com>
2023-06-01 12:49:24 -0500

    try/catch/finally

---
+++ src/main/java/org/example/Error_Excep/MyRunner.java
@@ -0,0 +1,19 @@
+package org.example.Error_Excep;
+
+public class MyRunner {
+    public static void main(String[] args) {
+        try{
+            int d = 0;
+            int n = 5;
+            // code that generates exception
+                int divideByZero = n / d;
+        }
+        catch(ArithmeticException e) {
+            System.out.println("ArithmeticException occur => " + e.getMessage());
+            e.printStackTrace();
+        }
+        finally{
+            System.out.println("This is the finally block");
+        }
+    }
+}
