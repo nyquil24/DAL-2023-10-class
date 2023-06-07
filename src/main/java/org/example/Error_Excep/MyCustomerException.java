ad7643c2ace9898f41040032eb2bba0407156840
Opie Taylor <taborn.nequil@gmail.com>
2023-06-06 19:31:06 -0500

    Custom exception

---
+++ src/main/java/org/example/Error_Excep/MyCustomerException.java
@@ -0,0 +1,17 @@
+package org.example.Error_Excep;
+
+public class MyCustomerException extends Exception {
+    private String str1;
+
+    /* Constructor of custom exception class here I am copying the message that we are
+    passing while throwing the exception to a string and then displaying that string along with
+    the message.
+     */
+
+    MyCustomerException(String str2) {
+        this.str1 = str2;
+    }
+    public String toString(){
+        return(" --->> MyException Occurred: " +str1);
+    }
+}
